65595155a58115eae8ee35fec2a8ea22b5799454
Maher-Amara <maheramara32@yahoo.fr>
2021-10-10 19:15:21 +0100

    set up css production pipeline

---
+++ purgecss.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    content: [
+        'public/*.html',
+        'public/js/*.js'
+    ],
+    css: [
+        'public/css/style.css'
+    ],
+    safelist: [],
+    output: "public/css/style.purge.css"
+  }
